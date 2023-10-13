466a76436e3d4513fdedb7417505ba70e6dc6203
Iftekhar Inan <inan.bd@gmail.com>
2023-10-13 08:23:31 +0600

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -7,7 +7,7 @@ import Wrapper from "@/layout/wrapper";
 const index = () => {
   return (
     <Wrapper>
-      <SEO pageTitle="Home Main" />
+      <SEO pageTitle="NSD Home" />
       <Layout>
         <HomeOne />
       </Layout>
